e6afc14645ae9fa1081f5e51058b6d4d6f8f3709
Александр Сырокласов <SiroklasovA@rkvv.local>
2020-06-05 20:57:59 +0300

    123

---
+++ src/components/CommentItem/CommentItem.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { useRadioGroup } from "@material-ui/core"
+export default class CommentItem extends React.Component{
+    
+    render()
+    {
+        text=this.props.data.text
+        [phone,mobile_phone,]
+        return(
+            <React.Fragment>
+                <Box>
+                    {}
+                </Box>
+            </React.Fragment>
+        )
+    }
+}
